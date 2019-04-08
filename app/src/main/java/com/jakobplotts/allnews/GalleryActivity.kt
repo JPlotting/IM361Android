53f7dfaa314c14548fbe4784c1acac1592b5c79a
Jakob Plotts <jakobplotts@yahoo.com>
2019-04-07 23:27:04 -0500

    Android project concluded

--- app/src/main/java/com/jakobplotts/allnews/GalleryActivity.kt
+++ app/src/main/java/com/jakobplotts/allnews/GalleryActivity.kt
@@ -1,11 +1,43 @@
 package com.jakobplotts.allnews
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+
+
+// Images taken royalty free from: https://www.pexels.com/
 
 class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+        // Grab references to each thumbnail in the gallery
+        val imageGalleryThumb1: ImageView = findViewById(R.id.ImageGalleryThumb1)
+        val imageGalleryThumb2: ImageView = findViewById(R.id.ImageGalleryThumb2)
+        val imageGalleryThumb3: ImageView = findViewById(R.id.ImageGalleryThumb3)
+
+        // Make the first thumbnail tappable so it goes to the article
+        imageGalleryThumb1.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            val options = ActivityOptions.makeSceneTransitionAnimation(this,imageGalleryThumb1,"ImageGalleryThumb1")
+            startActivity(intent,options.toBundle())
+        }
+
+        // Make the second thumbnail tappable so it goes to the article
+        imageGalleryThumb2.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            val options = ActivityOptions.makeSceneTransitionAnimation(this,imageGalleryThumb2,"ImageGalleryThumb2")
+            startActivity(intent,options.toBundle())
+        }
+
+        // Make the third thumbnail tappable so it goes to the article
+        imageGalleryThumb3.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            val options = ActivityOptions.makeSceneTransitionAnimation(this,imageGalleryThumb3,"ImageGalleryThumb3")
+            startActivity(intent,options.toBundle())
+        }
     }
 }
--- app/src/main/java/com/jakobplotts/allnews/MainActivity.kt
+++ app/src/main/java/com/jakobplotts/allnews/MainActivity.kt
@@ -2,60 +2,75 @@ package com.jakobplotts.allnews
 
 import android.app.ActivityOptions
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.animation.DynamicAnimation
 import android.support.animation.SpringAnimation
+import android.support.animation.SpringForce
+import android.support.design.widget.FloatingActionButton
 import android.widget.Button
-import android.widget.TextView
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
-import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    override fun onMapReady(map: GoogleMap?) {
-        val morton = LatLng(40.6091693,-89.5392298)
-        map?.addMarker(MarkerOptions().position(morton).title("Pumpkin capital of the world"))
-        map?.moveCamera(CameraUpdateFactory.newLatLng(morton))
-    }
-
-    var counter = 0
+class MainActivity : AppCompatActivity() {
+    // Current state of the fav action button
+    private var articleFaved = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText: TextView = findViewById(R.id.my_text_view)
-        val myButton: Button = findViewById(R.id.my_button)
-        val myButtonViewTheirContent: Button = findViewById(R.id.button_view_their_content)
-        val anim = SpringAnimation(myText,DynamicAnimation.TRANSLATION_Y,-48f)
-        anim.setStartValue(0f)
+        // App bar setup with help from: https://developer.android.com/training/appbar/setting-up
+        setSupportActionBar(findViewById(R.id.ToolbarArticle))
 
-        myButton.setOnClickListener {
-            counter++
-            myText.text = resources.getQuantityString(R.plurals.textView_text,counter,counter)
-            anim.start()
-        }
+        // Grab element references
+        val floatingFavButton: FloatingActionButton = findViewById(R.id.FloatingFavButton)
+        val buttonViewMore: Button = findViewById(R.id.ButtonViewMore)
 
-        myButtonViewTheirContent.setOnClickListener {
+        // Create a custom spring animation to cause a hop animation for pressing the fav button
+        // Assistance/code from: https://code.tutsplus.com/tutorials/adding-physics-based-animations-to-android-apps--cms-29053
+        val favSpring: SpringForce = SpringForce()
+        favSpring.setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY)
+        favSpring.setStiffness(SpringForce.STIFFNESS_LOW)
+
+        // Attaching custom spring to animation
+        val favAnim = SpringAnimation(floatingFavButton, DynamicAnimation.Y)
+        favAnim.setSpring(favSpring)
+
+        // Set a listener on the "view more" button so that it will take the user to the gallery activity
+        // Transition from in class material
+        buttonViewMore.setOnClickListener {
             val intent = Intent(this,GalleryActivity::class.java)
-            val options = ActivityOptions.makeSceneTransitionAnimation(this,myText,"image_gallery_example")
+            val options = ActivityOptions.makeSceneTransitionAnimation(this,buttonViewMore,"ImageGalleryThumb1")
             startActivity(intent,options.toBundle())
         }
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.my_map_view) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        // Animated fav button upon tapping (it toggles states but that is it)
+        // Physics animations according to course material
+        floatingFavButton.setOnClickListener {
+            // Make sure to only allow tapping when not being animated (no physics stacking!)
+            if (!favAnim.isRunning) {
+                // Toggle the state of the fav button and alter the tint to reflect it
+                // NOTE: I used getResources because it was the only way I could get it to work for Kotlin but says it has been deprecated for Java so hopefully this is okay
+                if (articleFaved == 0) {
+                    articleFaved = 1
+                    floatingFavButton.setColorFilter(getResources().getColor(R.color.colorButtonActive))
+                } else {
+                    articleFaved = 0
+                    floatingFavButton.setColorFilter(getResources().getColor(R.color.colorButtonInactive))
+                }
+
+                // Start brief hop animation
+                favSpring.setFinalPosition(floatingFavButton.y)
+                favAnim.setStartVelocity(1000f)
+                favAnim.start()
+            }
+        }
     }
+
+    // VVV Methods placed by default layout to fill action bar with basic functionality (more/settings) VVV
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
